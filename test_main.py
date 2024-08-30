8f794d60e89db843fadc859e1b8f8eaf2c1e0a0a
Jennifer Li <jenniferli@Jennifers-MacBook-Air-2.local>
2024-08-30 15:14:36 -0400

    updated files

--- test_main.py
+++ test_main.py
@@ -5,4 +5,15 @@ def test_add():
     """testing out add function"""
     assert add(2, 2) == 4
     assert add(3, 2) == 5
-    assert add(1, 3) == 4
+    print("hello")
+
+
+def test_add2():
+    """testing out add function"""
+    assert add(2, 2) == 4
+    assert add(3, 2) == 5
+    assert add(1, 2) == 3
+
+
+if __name__ == "__main__":
+    test_add()
